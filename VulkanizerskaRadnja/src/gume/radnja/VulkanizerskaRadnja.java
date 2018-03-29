35f525196b24dc78675a17da8d86eee5e824e82e
Jasmina-Stevic <stevic9616@fon.bg.ac.rs>
2018-03-29 11:58:32 +0200

    Testirana je klasa VulkanizerskaRadnja i ispravljene su greske u njoj.

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -6,6 +6,7 @@ import gume.AutoGuma;
 
 /**
  * Klasa koja predstavlja vulkanizersku radnju.
+ * 
  * @author Jasmina Stevic
  * @version 1.0
  *
@@ -15,31 +16,41 @@ public class VulkanizerskaRadnja {
 	 * Lista guma koja sadrzi objekte klase AutoGuma
 	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
-/**
- * Metoda koja dodaje novu gumu u listu 
- * @param a prestavlja novu gumu u listi
- * @throws java.lang.NullPointerException ako je prosledjeni objekat null
- * @throws java.lang.RuntimeException ako takva guma vec postoji u listi
- */
+
+	/**
+	 * Metoda koja dodaje novu gumu u listu
+	 * 
+	 * @param a
+	 *            prestavlja novu gumu u listi
+	 * @throws java.lang.NullPointerException
+	 *             ako je prosledjeni objekat null
+	 * @throws java.lang.RuntimeException
+	 *             ako takva guma vec postoji u listi
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
+
 		if (gume.contains(a))
 			throw new RuntimeException("Guma vec postoji");
+
 		gume.addFirst(a);
 	}
-/**
- * Metoda koja pronalazi trazenu gumu po nazivu marke i modela u listi svih guma
- * @param markaModel naziv marke i modela gume kao String
- * @return nova lista sa gumama trazene marke i modela,ako takve postoje u listi
- * @return null ako ne postoji guma trazene marke i modela u listi
- */
+
+	/**
+	 * Metoda koja pronalazi trazenu gumu po nazivu marke i modela u listi svih guma
+	 * 
+	 * @param markaModel
+	 *            naziv marke i modela gume kao String
+	 * @return nova lista sa gumama trazene marke i modela,ako takve postoje u listi
+	 * @return null ako ne postoji guma trazene marke i modela u listi
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
 		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
 		for (int i = 0; i < gume.size(); i++)
-			if (gume.get(i).equals(markaModel))
+			if (gume.get(i).getMarkaModel().equals(markaModel)) //nedostajalo je .getMarkaModel
 				novaLista.add(gume.get(i));
 		return novaLista;
 	}
---
+++ VulkanizerskaRadnja/test/gume/radnja/VulkanizerskaRadnjaTest.java
@@ -0,0 +1,147 @@
+/**
+ * 
+ */
+package gume.radnja;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import gume.AutoGuma;
+
+/**
+ * @author JasminaStevic
+ *
+ */
+public class VulkanizerskaRadnjaTest {
+	VulkanizerskaRadnja r;
+	AutoGuma a;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	
+	@Before
+	public void setUp() throws Exception {
+		r = new VulkanizerskaRadnja();
+		a = new AutoGuma();
+	}
+
+	
+	@After
+	public void tearDown() throws Exception {
+		r = null;
+		a= null;
+	}
+
+	
+	@Test
+	public void testDodajGumu() {
+		a.setMarkaModel("Michelin Alpin 5");
+		a.setPrecnik(20);
+		a.setSirina(200);
+		a.setVisina(60);
+		r.dodajGumu(a);
+	}
+	
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testDodajISTUGumu() {
+		a.setMarkaModel("Michelin Alpin 5");
+		a.setPrecnik(20);
+		a.setSirina(200);
+		a.setVisina(60);
+		r.dodajGumu(a);
+		
+		AutoGuma a2 = new AutoGuma();
+		a2.setMarkaModel("Michelin Alpin 5");
+		a2.setPrecnik(20);
+		a2.setSirina(200);
+		a2.setVisina(60);
+		r.dodajGumu(a2);
+	}
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testDodajGumuNull() {
+		a = null;
+		r.dodajGumu(a);
+	}
+	
+	@Test
+	public void testPronadjiGumuIspravno() {
+		a.setMarkaModel("Michelin Alpin 5");
+		a.setPrecnik(20);
+		a.setSirina(200);
+		a.setVisina(60);
+		r.dodajGumu(a);
+		assertEquals(1, r.pronadjiGumu("Michelin Alpin 5").size());
+	}
+	@Test
+	public void testPronadjiGumuNema() {
+		a.setMarkaModel("Michelin Alpin 5");
+		a.setPrecnik(20);
+		a.setSirina(200);
+		a.setVisina(60);
+		r.dodajGumu(a);
+		assertEquals(0, r.pronadjiGumu("Alpa").size());
+	}
+	@Test
+	public void testPronadjiGumuNull() {
+		assertEquals(null, r.pronadjiGumu(null));
+	}
+
+	@Test
+	public void testPronadjiGumuPronalaziNekoliko() {
+		a.setMarkaModel("Michelin Alpin 5");
+		a.setPrecnik(20);
+		a.setSirina(200);
+		a.setVisina(60);
+		r.dodajGumu(a);
+		
+		AutoGuma a2 = new AutoGuma();
+		a2.setMarkaModel("Michelin Alpin 5");
+		a2.setPrecnik(21);
+		a2.setSirina(200);
+		a2.setVisina(58);
+		r.dodajGumu(a2);
+		
+		AutoGuma a3 = new AutoGuma();
+		a3.setMarkaModel("Michelin Samsung 42");
+		a3.setPrecnik(15);
+		a3.setSirina(225);
+		a3.setVisina(45);
+		r.dodajGumu(a3);
+		
+		AutoGuma a4 = new AutoGuma();
+		a4.setMarkaModel("Michelin Alpin 5");
+		a4.setPrecnik(18);
+		a4.setSirina(210);
+		a4.setVisina(50);
+		r.dodajGumu(a4);
+		
+		AutoGuma a5 = new AutoGuma();
+		a5.setMarkaModel("Michelin Samsung 42");
+		a5.setPrecnik(14);
+		a5.setSirina(215);
+		a5.setVisina(48);
+		r.dodajGumu(a5);
+		
+		AutoGuma a6 = new AutoGuma();
+		a6.setMarkaModel("Michelin Alpin 5");
+		a6.setPrecnik(17);
+		a6.setSirina(211);
+		a6.setVisina(49);
+		r.dodajGumu(a6);
+		assertEquals(4, r.pronadjiGumu("Michelin Alpin 5").size());
+	}
+	
+	
+}
